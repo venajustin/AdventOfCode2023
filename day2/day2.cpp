9032560c3e5e266879366a1dfa96ac59a728a6e8
venajustin <venajustin@gmail.com>
2023-12-07 16:13:19 -0800

    Day 2 completed

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,9 +1,13 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <string>
 
+const int MAX_RED = 12;
+const int MAX_GREEN = 13;
+const int MAX_BLUE = 14;
 
 using namespace std;
-
 
 int main( int argc, char *argv[]) {
 	
@@ -18,90 +22,80 @@ int main( int argc, char *argv[]) {
 		cout << "File could not be found!" << endl;
 		return 1;
 	}
-	
-	string input;
-	
-	int r = 12;
-	int g = 13;
-	int b = 14;
-	
-	int rmax = 0;
-	int gmax = 0;
-	int bmax = 0;
-	
-	int gameid = 0;
-	
-	int sum = 0;
-	
-	string temp;
-	
-	
-	cin >> input;
-	while (!my_file.eof()) {
-		
-		if ( input != "Game" ) {
-			cout << "error" << endl;
-		}
-		
-		
-		cin >> temp;
-		gameid = 0;
-		for (int i = 0; i < temp.length() - 1; i++) {
-			gameid *= 10;
-			gameid += temp[i];
-		}
-		
-		int rmax = 0;
-		int gmax = 0;
-		int bmax = 0;
-	
-		input = "," ;
-		while (input[input.length()] == ',' || input[input.length()] == ';') {
-		
-			int numin;
-			
-			cin >> numin;
-			cout << numin;
-			cin >> input;
-			cout << " " << input << endl;
-			
-			char firstchar = input[0];
-			
-			switch (firstchar) {
-				case 'r':
-					if (numin > rmax) {
-						rmax = numin;
-					}
-					break;
-				case 'g':
-					if (numin > gmax) {
-						gmax = numin;
-					}
-					break;
-				case 'b':
-					if (numin > bmax) {
-						bmax = numin;
-					}
-					break;
-				default:
-					return -1;
-					break;
-			}
-		}
-		
-		cout << "Game " << gameid << ": " << "rmax: " << rmax << " gmax: " << gmax << " bmax: " << bmax << endl;
-		if (rmax >= r && gmax >= g && bmax >= b) {
-			sum += gameid;
-			cout << "Game accepted" << endl;
-		}
-		
-	}
-	
-	if (rmax >= r && gmax >= g && bmax >= b) {
-		sum += gameid;
-	}
-	
-	
+
+
+    string input;
+
+    int sum_part1 = 0;
+    int sum_part2 = 0;
+    int gameid = 0;
+    int game_state = 0;
+
+    int rmax = 0;
+    int gmax = 0;
+    int bmax = 0;
+
+    while (my_file >> input) {
+
+        cout << input << endl;
+
+        if (input == "Game") {
+            // checking the last game for errors (part 1 only)
+            if (rmax > MAX_RED  || gmax > MAX_GREEN || bmax > MAX_BLUE) {
+                sum_part1 -= gameid;
+            }
+            int power = rmax * gmax * bmax;
+            sum_part2 += power;
+
+            gameid++;
+            sum_part1 += gameid;
+            my_file >> input;
+            rmax = 0;
+            gmax = 0;
+            bmax = 0;
+            continue;
+        }
+
+        char* endptr;
+        int num = stoi(input);
+        my_file >> input;
+        char color = input[0];
+
+        switch (color) {
+            case 'r':
+                if (num > rmax) {
+                    rmax = num;
+                }
+                break;
+            case 'g':
+                if (num > gmax) {
+                    gmax = num;
+                }
+                break;
+            case 'b':
+                if (num > bmax) {
+                    bmax = num;
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    if (rmax > MAX_RED  || gmax > MAX_GREEN || bmax > MAX_BLUE) {
+        sum_part1 -= gameid; 
+    }
+    int power = rmax * gmax * bmax;
+    sum_part2 += power;
+
+
+
+    cout << "Final Sum of valid game ids: " << sum_part1 << endl;
+
+    cout << "Final Sum of power sets: " << sum_part2 << endl;
+
+
 	my_file.close();
 	return 0;
 }
